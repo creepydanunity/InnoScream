a73053b30cd7841f0efbf37d26eefcedb53d6f34
Daniil Vasilev <mrcreepydanx@gmail.com>
2025-04-19 13:01:26 +0300

    Modified .md, .gitignore

---
+++ app_fastapi/models/user_feed.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, Integer, String
+from app_fastapi.models.base import Base
+
+class UserFeedProgress(Base):
+    __tablename__ = "user_feed_progress"
+
+    user_hash = Column(String, primary_key=True)
+    last_seen_id = Column(Integer, default=0)
