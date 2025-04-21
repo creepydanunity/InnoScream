00f7acd89e4214919ea78399595717e472f28b6e
Ekaterina Zaitseva <katy.zaitsewa2207@ya.ru>
2025-04-21 22:35:41 +0300

    startHandler implemented

--- app_bot/main.py
+++ app_bot/main.py
@@ -4,7 +4,7 @@ from aiogram import Bot, Dispatcher
 from app_bot.handlers.reactionHandler import reactionRouter
 from app_bot.handlers.statsHandler import statsRouter
 from app_bot.handlers.screamHandler import screamRouter
-
+from app_bot.handlers.startHandler import startRouter
 
 async def main():
     bot = Bot(token=os.getenv("BOT_TOKEN"))
@@ -12,10 +12,9 @@ async def main():
     dp.include_router(reactionRouter)
     dp.include_router(screamRouter)
     dp.include_router(statsRouter)
+    dp.include_router(startRouter)
 
     await dp.start_polling(bot, polling_timeout=120, skip_updates=False)
 
 if __name__ == "__main__":
     asyncio.run(main())
-
-
