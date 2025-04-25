5ad9a00c76624269dbbe7654c5bc93372f58f45f
NikaChek <nicachehonina@gmail.com>
2025-04-25 10:47:42 +0300

    dockstrings

--- app_bot/handlers/getIdHandler.py
+++ app_bot/handlers/getIdHandler.py
@@ -6,6 +6,23 @@ idRouter = Router()
 
 @idRouter.message(Command("my_id"))
 async def handle_my_id(msg: types.Message):
+
+    """
+    Handles the '/my_id' command to retrieve and send the user's ID.
+
+    When the user sends the '/my_id' command, the bot will:
+    - Extract the user ID from the message.
+    - Call the `get_my_id` function to retrieve the user ID from the API.
+    - Respond with the retrieved user ID if successful.
+    - If there is an error, respond with a failure message.
+
+    Args:
+        msg (types.Message): The message object containing the user's message and information.
+
+    Returns:
+        None
+    """
+
     user_id = str(msg.from_user.id) 
 
     try:
