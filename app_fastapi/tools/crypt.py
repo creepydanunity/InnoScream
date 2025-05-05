82d54a124fc35753152013093c1e794030a0448e
Daniil Vasilev <creepydanunity@gmail.com>
2025-05-05 16:50:18 +0300

    Added Global Salting

--- app_fastapi/tools/crypt.py
+++ app_fastapi/tools/crypt.py
@@ -1,13 +1,16 @@
 # Standard library
 import hashlib
 import logging
+import os
 
 
 logger = logging.getLogger("app_fastapi.tools")
+USER_ID_SALT = os.getenv("USER_ID_SALT")
+
 
 def hash_user_id(user_id: int) -> str:
     """
-    Compute a SHA-256 hash of the given user ID.
+    Compute a salted SHA-256 hash of the given user ID.
 
     This function:
       - Accepts a numeric user identifier.
@@ -19,15 +22,23 @@ def hash_user_id(user_id: int) -> str:
         user_id (int): The external user identifier to hash.
 
     Returns:
-        str: Hexadecimal SHA-256 hash of the input user ID.
+        str: Hexadecimal salted SHA-256 hash of the input user ID.
 
     Raises:
         Exception: If any error occurs during hashing, it is logged and re-raised.
     """
     try:
         logger.debug(f"Hashing user ID: {user_id}")
-        hashed = hashlib.sha256(str(user_id).encode()).hexdigest()
-        logger.debug(f"Hashed result: {hashed[:5]}...")
+
+        if not USER_ID_SALT:
+            raise ValueError("USER_ID_SALT not found in environment variables")
+        
+        salted_user_id = f"{USER_ID_SALT}{user_id}"
+
+        hashed = hashlib.sha256(salted_user_id.encode()).hexdigest()
+        
+        logger.debug(f"Hashed result: {salted_user_id[:10]}..., Hashed result: {hashed[:5]}...")
+
         return hashed
     except Exception as e:
         logger.error(f"Failed to hash user ID: {str(e)}", exc_info=True)
